dd1cea86b4d1c836708b46e7366fdccfdd80e371
Hasan Baig <hasanbaig2000@gmail.com>
2020-09-27 03:27:43 -0400

    feat: Completed step 1 of hardware

---
+++ Lab-2/lab2-hardware-step1.py
@@ -0,0 +1,22 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+  
+sense.clear()
+
+#selection helps switch between letters every loop
+selection = False     
+
+while True: 
+  events = sense.stick.get_events()
+  for event in events:
+    if event.action == "pressed":
+      if selection == False:
+        # Show first intial 
+        sense.show_letter("H")
+        selection = True
+      else:
+        # Show last intial
+        sense.show_letter("B")
+        selection = False

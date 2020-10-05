c08e59048d3cbe004c577477d88961976ddf76b6
Hasan Baig <hasanbaig2000@gmail.com>
2020-10-05 15:08:46 -0400

    feat: updated comment

--- Lab-4/lab4.py
+++ Lab-4/lab4.py
@@ -5,7 +5,7 @@ import time
 key = "54CDA6FJF4RCJ92C"
         
 if __name__ == "__main__":
-    #Write email address, project group and member identifier
+    #Write email address, project group and member identifier to ThingSpeak channel 
     params = urllib.parse.urlencode({'field1': "hasanbaig@cmail.carleton.ca", 'field2': "L2-M-5",'field3': "a", 'key': key})
     headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
     conn = http.client.HTTPConnection("api.thingspeak.com:80")
